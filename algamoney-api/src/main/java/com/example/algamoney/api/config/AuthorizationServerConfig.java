b21e1b3ac7a04b84ab2e038481620c2f81060a54
Danilo Arruda <da.ni.lo_arruda@outlook.com>
2017-10-06 13:27:40 -0300

    CORS implementado

--- algamoney-api/src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -8,9 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -27,14 +25,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.withClient("angular")
 				.secret("@ngul@r0")
 				.scopes("read", "write")
-				.authorizedGrantTypes("password")
-				.accessTokenValiditySeconds(1800);
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(3600) // 1h
+				.refreshTokenValiditySeconds(3600 * 24); // 1 dia
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
 					.accessTokenConverter(accessTokenConverter())
+					.reuseRefreshTokens(false)
 					.authenticationManager(authenticationManager);
 					
 	}
